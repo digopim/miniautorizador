d02f7b88d39020d8c01c9b364fce386400d954e1
digopim <digopim@gmail.com>
2023-01-21 19:24:25 -0300

    Criando projeto Kotlin + mongodb

---
+++ miniautorizador/src/main/kotlin/com/vr/miniautorizador/MiniautorizadorApplication.kt
@@ -0,0 +1,11 @@
+package com.vr.miniautorizador
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniautorizadorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniautorizadorApplication>(*args)
+}
