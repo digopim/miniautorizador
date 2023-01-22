e6a1538599eb6fccd7a920dda00dcee0a4dad75c
digopim <digopim@gmail.com>
2023-01-22 01:09:19 -0300

    transacao simples sem validacao

--- miniautorizador/src/main/kotlin/com/vr/miniautorizador/controller/TransacaoController.kt
+++ miniautorizador/src/main/kotlin/com/vr/miniautorizador/controller/TransacaoController.kt
@@ -1,5 +1,30 @@
 package com.vr.miniautorizador.controller
 
-class TransacaoController (){
+import com.vr.miniautorizador.model.Cartao
+import com.vr.miniautorizador.model.Transacao
+import com.vr.miniautorizador.repository.CartaoRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/transacoes")
+class TransacaoController (val repository: CartaoRepository){
+
+
+    @PutMapping
+    fun update(@RequestBody transacao: Transacao)  : ResponseEntity<Cartao> {
+        val cartaoDB = repository.findById(transacao.numeroCartao);
+        return if(cartaoDB.isPresent){
+            val novoSaldo = cartaoDB.get().saldo?.minus(transacao.valor)
+            val aux = cartaoDB.get().copy(saldo = novoSaldo)
+            ResponseEntity.ok(repository.save(aux))
+        } else {
+            ResponseEntity.unprocessableEntity().build()
+        }
+    }
+
 
 }
--- miniautorizador/src/main/kotlin/com/vr/miniautorizador/model/Transacao.kt
+++ miniautorizador/src/main/kotlin/com/vr/miniautorizador/model/Transacao.kt
@@ -1,4 +1,7 @@
 package com.vr.miniautorizador.model
 
-class Transacao {
-}
+class Transacao (
+    val numeroCartao: Long,
+    val senhaCartao: String,
+    val valor: Long
+)
